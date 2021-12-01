9dbf9bd7860b4590835ff55efa46690e79b6cd29
Valera Rozuvan <valera@rozuvan.net>
2021-12-01 05:07:38 +0200

    Adding zen mode - part 2

--- js/plus-minus-input.js
+++ js/plus-minus-input.js
@@ -1,1 +1,27 @@
-jQuery(document).ready(function(){$('[data-quantity="plus"]').click(function(a){a.preventDefault(),fieldName=$(this).attr("data-field");var e=parseInt($("input[name="+fieldName+"]").val());isNaN(e)?$("input[name="+fieldName+"]").val(0):$("input[name="+fieldName+"]").val(e+1)}),$('[data-quantity="minus"]').click(function(a){a.preventDefault(),fieldName=$(this).attr("data-field");var e=parseInt($("input[name="+fieldName+"]").val());!isNaN(e)&&e>0?$("input[name="+fieldName+"]").val(e-1):$("input[name="+fieldName+"]").val(0)})});
+jQuery(document).ready(function () {
+    $('[data-quantity="plus"]').click(function (a) {
+        a.preventDefault();
+        var fieldName = $(this).attr("data-field");
+        var el = $("input[name=" + fieldName + "]");
+        var e = parseInt(el.val());
+        isNaN(e) ? el.val(0) : el.val(e + 1);
+    });
+
+    $('[data-quantity="minus"]').click(function (a) {
+        a.preventDefault();
+        var fieldName = $(this).attr("data-field");
+        var el = $("input[name=" + fieldName + "]");
+        var e = parseInt(el.val());
+        (!isNaN(e) && e > 0) ? el.val(e - 1) : el.val(0);
+    });
+
+    $('#delete_02').click(function (a) {
+        a.preventDefault();
+
+        ["#delete_00", "#delete_01", "#delete_02", "#delete_03", "#delete_04"].forEach(function (id) {
+            $(id).remove();
+        });
+
+        $("#counter").addClass("centered");
+    });
+});
