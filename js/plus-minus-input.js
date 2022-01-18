94183e01edc05695a7c0a9fcfcac06178ab61a58
Valera Rozuvan <valera@rozuvan.net>
2022-01-18 12:10:08 +0200

    fix UI issue
    - don't increase/decrease twice if the input element has focus
    - minor code re-factor

--- js/plus-minus-input.js
+++ js/plus-minus-input.js
@@ -1,18 +1,6 @@
-function increaseCounter(event) {
-    event.preventDefault();
-    var el = $("input[name=quantity]");
-    var e = parseInt(el.val());
-    isNaN(e) ? el.val(0) : el.val(e + 1);
-}
+window.jQuery(document).ready(function (_jquery) {
+    const $ = _jquery;
 
-function decreaseCounter(event) {
-    event.preventDefault();
-    var el = $("input[name=quantity]");
-    var e = parseInt(el.val());
-    (!isNaN(e) && e > 0) ? el.val(e - 1) : el.val(0);
-}
-
-jQuery(document).ready(function () {
     $('[data-quantity="plus"]').click((event) => {
         increaseCounter(event);
     });
@@ -32,10 +20,42 @@ jQuery(document).ready(function () {
     });
 
     $(document).on('keyup', (event) => {
+        if (
+            event &&
+            event.currentTarget &&
+            event.currentTarget.activeElement &&
+            event.currentTarget.activeElement.id === "input-group-field"
+        ) {
+            // We don't want to trigger a decrease/increase twice.
+            // Default input element (type="number") has a built-in handler for "ArrowUp" and "ArrowDown" keyboard events.
+            // That's why we skip processing keyboard events if the user is inside (has focused) the input element.
+            return;
+        }
+
         if (event.key === "ArrowUp") {
             increaseCounter(event);
         } else if (event.key === "ArrowDown") {
             decreaseCounter(event);
         }
     });
+
+    function increaseCounter(event) {
+        event.preventDefault();
+        event.stopPropagation();
+
+        const el = $("input[name=quantity]");
+        const oldValue = parseInt(el.val());
+
+        isNaN(oldValue) ? el.val(0) : el.val(oldValue + 1);
+    }
+
+    function decreaseCounter(event) {
+        event.preventDefault();
+        event.stopPropagation();
+
+        const el = $("input[name=quantity]");
+        const oldValue = parseInt(el.val());
+
+        (!isNaN(oldValue) && oldValue > 0) ? el.val(oldValue - 1) : el.val(0);
+    }
 });
